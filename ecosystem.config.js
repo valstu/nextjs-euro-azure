8b66a60aabcb241796fb906e2034bccf12287fad
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-08-29 22:42:50 +0300

    PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app-name",
-      script: "./node_modules/.bin/next",
+      script: "./next/standalone/node_modules/next next",
       args: "start -p " + (process.env.PORT || 3000),
       watch: false,
       autorestart: true,
