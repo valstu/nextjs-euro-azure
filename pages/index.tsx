d84c302417e94b40f524623e75384983ee0d3aa0
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-09-01 00:53:01 +0300

    Minor updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! AZURE!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! Hee</a>
         </h1>
 
         <p className={styles.description}>
