98c16aa9f60b955b4ac3e8ca58b52dccb89dd5cb
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-09-01 00:12:40 +0300

    New pipeline

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePaths: "/nextjsserver",
-  // output: "standalone",
+  output: "standalone",
 };
 
 module.exports = nextConfig;
