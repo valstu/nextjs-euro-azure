a272991cc3de4d26eefa218abbef06ddd7746d9e
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-09-01 17:04:43 +0300

    Added assetprefix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
   experimental: { images: { allowFutureImage: true } },
+  assetPrefix: isProd
+    ? "https://nextjs-euro-dkh8g6eybgfsgpdd.z01.azurefd.net/"
+    : undefined,
   images: {
     domains: ["images.unsplash.com"],
   },
